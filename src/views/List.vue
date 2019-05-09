0509b97a92a94e5277855ce1ff0fdc2a8a4f32bd
guo1901 <2548678960@qq.com>
2019-05-09 17:19:14 +0800

    压缩包

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,85 +1,104 @@
 <template>
-    <div>
-        <el-header style="height:0.44rem;padding:0;">
-        <header class="mint-header cart">
-          <div class="mint-header-button is-left">
-            <a href="#/" class="router-link-active">
-              <button class="mint-button mint-button--default mint-button--normal">
-                <span class="mint-button-icon">
-                  <i class="mintui mintui-back"></i>
-                </span>
-                <label class="mint-button-text"></label>
-              </button>
-            </a>
-          </div>
-          <el-col :span="16">
-            <el-input placeholder="洪湖渔家小龙虾49.9元" prefix-icon="el-icon-search" class="mysearch"></el-input>
-          </el-col>
-
-          <div class="mint-header-button is-right">
-              <span class="cart-icon">购物车</span>
+  <div>
+    <el-header style="height:0.44rem;padding:0;">
+      <header class="mint-header cart">
+        <div class="mint-header-button is-left">
+          <a href="#/" class="router-link-active">
             <button class="mint-button mint-button--default mint-button--normal">
               <span class="mint-button-icon">
-                <i class="mintui mintui-more"></i>
+                <i class="mintui mintui-back"></i>
               </span>
               <label class="mint-button-text"></label>
             </button>
-          </div>
-        </header>
-      </el-header>
+          </a>
+        </div>
+        <el-col :span="16">
+          <el-input placeholder="洪湖渔家小龙虾49.9元" prefix-icon="el-icon-search" class="mysearch"></el-input>
+        </el-col>
 
-        <!-- <ul>
-           <li v-for="item in navs" :key="item.name" @click="goto(item.path)">{{item.text}}</li>  
-        </ul> -->
-    </div>
+        <div class="mint-header-button is-right">
+          <span class="cart-icon">购物车</span>
+          <button class="mint-button mint-button--default mint-button--normal">
+            <span class="mint-button-icon">
+              <i class="mintui mintui-more"></i>
+            </span>
+            <label class="mint-button-text"></label>
+          </button>
+        </div>
+      </header>
+    </el-header>
+
+    <el-table
+    :data="tableData"
+    style="width: 100%"
+    :default-sort = "{prop: 'date', order: 'descending'}"
+    >
+    <el-table-column
+      prop="date"
+      label="综合"
+      sortable
+      width="90">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="价格"
+      sortable
+      width="90">
+    </el-table-column>
+     <el-table-column
+      prop="name"
+      label="销量"
+      sortable
+      width="90">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="筛选"
+      :formatter="formatter">
+    </el-table-column>
+  </el-table>
+  </div>
 </template>
 <script>
-    export default{
-        data(){
-            return {
-            navs:[{
-                name:'Phone',
-                path:'/list/phone',
-                text:'手机'
-
-            },{
-                name:'Computer',
-                path:'/list/computer',
-                text:'电脑'
-            },{
-                name:'Pad',
-                path:'/list/pad',
-                text:'平板'
-            }],
-        }
+    export default {
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      }
     },
-    mounted(){
-    },
-    methods:{
-        goto(path){
-            // push 有浏览器记录
-            // this.$router.push(path)
-
-            // replace 无浏览器记录。
-            this.$router.replace(path);
-        }
+    methods: {
+      formatter(row, column) {
+        return row.address;
+      }
     }
-}
-
-// this.$watch('$route')
+  }
 </script>
 
 <style lang="scss" scoped>
 .el-header {
-    .cart{
-      width: 100%;
-      height: 0.44rem;
-      font: 400 0.18rem/0.44rem "黑体";
-      color: #000;
-      background: #fff;
-      border-bottom: 1px solid #ccc;
-      
-    }
+  .cart {
+    width: 100%;
+    height: 0.44rem;
+    font: 400 0.18rem/0.44rem "黑体";
+    color: #000;
+    background: #fff;
+    border-bottom: 1px solid #ccc;
   }
+}
 </style>
-
