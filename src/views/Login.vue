d641755fe5809765ac1c08085c03b14df055928b
jjy <171741674@qq.com>
2019-05-10 20:22:44 +0800

    注册功能完成

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,39 +4,63 @@
     <el-tab-pane label="注册">
       <!-- <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign"> -->
       <el-form
-        :model="ruleForm"
+        :model="ruleForm_reg"
         :rules="rules"
-        ref="ruleForm"
+        ref="ruleForm_reg"
         label-width="80px"
-        class="demo-ruleForm"
+        class="demo-ruleForm_reg"
       >
         <el-form-item label="用户名" prop="username">
-          <el-input v-model="ruleForm.username"></el-input>
+          <el-input v-model="ruleForm_reg.username"></el-input>
         </el-form-item>
         <el-form-item label="密码" prop="psw">
-          <el-input v-model="ruleForm.psw"></el-input>
+          <el-input v-model="ruleForm_reg.psw"></el-input>
         </el-form-item>
         <el-form-item label="确认密码" prop="comfirmPsw">
-          <el-input v-model="ruleForm.comfirmPsw"></el-input>
+          <el-input v-model="ruleForm_reg.comfirmPsw"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-          <el-button @click="resetForm('ruleForm')">重置</el-button>
+          <el-button type="primary" @click="submitForm('ruleForm_reg')">提交</el-button>
+          <el-button @click="resetForm('ruleForm_reg')">重置</el-button>
         </el-form-item>
       </el-form>
     </el-tab-pane>
-    <el-tab-pane label="登陆">配置管理</el-tab-pane>
+    <el-tab-pane label="登陆">
+      <el-form
+        :model="ruleForm_reg"
+        :rules="rules"
+        ref="ruleForm_reg"
+        label-width="80px"
+        class="demo-ruleForm_reg"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="ruleForm_reg.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="psw">
+          <el-input v-model="ruleForm_reg.psw"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="comfirmPsw">
+          <el-input v-model="ruleForm_reg.comfirmPsw"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm_reg')">提交</el-button>
+          <el-button @click="resetForm('ruleForm_reg')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-tab-pane>
   </el-tabs>
 </template>
 
 <script>
+import CryptoJS from "crypto-js";
+
 export default {
   data() {
     // value为comfirm的值
     let checkPsw = (rule, value, callback) => {
       if (value === "") {
         callback(new Error("请再次输入密码"));
-      } else if (value !== this.ruleForm.psw) {
+      } else if (value !== this.ruleForm_reg.psw) {
         callback(new Error("两次输入密码不一致!"));
       } else {
         callback();
@@ -45,25 +69,23 @@ export default {
 
     // 向后端校验用户名是否存在
     let checkUsername = (rule, value, callback) => {
-      this.axios
+      this.$axios
         .get("http://193.112.60.97:19011/reg/check", {
           params: {
             username: value
           }
         })
-        .then(res => {
-          console.log("reg", res);
+        .then(({ data }) => {
+          if (data.status === 200) {
+            //直接调用回调函数==放行
+            callback();
+          } else if (data.status === 400) {
+            callback(new Error("用户名已存在"));
+          }
         });
-      // if (value === "") {
-      //   callback(new Error("请再次输入密码"));
-      // } else if (value !== this.ruleForm.psw) {
-      //   callback(new Error("两次输入密码不一致!"));
-      // } else {
-      //   callback();
-      // }
     };
     return {
-      ruleForm: {
+      ruleForm_reg: {
         username: "",
         psw: "",
         comfirmPsw: ""
@@ -101,11 +123,30 @@ export default {
   methods: {
     submitForm(formName) {
       this.$refs[formName].validate(valid => {
-        console.log(valid);
+        // console.log(valid);
         if (valid) {
           this.$message("注册成功");
+          let { username, psw: password } = this.ruleForm_reg;
+
+          console.log("加密前：", password);
+          // 对password进行加密
+          let key = "laoxie1234567890"; //密钥
+          let iv = "laoxielaoxie6666"; //初始向量
+
+          key = CryptoJS.enc.Utf8.parse(key);
+          iv = CryptoJS.enc.Utf8.parse(iv);
+          var encrypted = CryptoJS.AES.encrypt(password, key, { iv });
+          password = encrypted.toString(); //返回的是base64格式的密文（后端要与之匹配）
+          console.log("加密后：", password);
+
+          this.$axios
+            .post("http://193.112.60.97:19011/reg", { username, password })
+            .then(({ data }) => {
+              console.log(data);
+              // this.$router.replace("/login");
+            });
         } else {
-          console.log("error submit!!");
+          // console.log("error submit!!");
           return false;
         }
       });
